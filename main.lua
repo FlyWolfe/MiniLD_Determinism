75913d1718bbb133874e5bf9aa0fb4c77c242103
Lukas Hillmer <lukas.hillmer@gmail.com>
2016-09-17 00:03:46 -0400

    added disappearing platforms,which will disappear 1.5s after first being landed on. cleaned up spacing

--- main.lua
+++ main.lua
@@ -1,40 +1,52 @@
 require ("platform")
 require ("player")
 function love.load()
-  --initial graphics setup
-  love.graphics.setBackgroundColor(104, 136, 248) --set the background color to a nice blue
-  love.window.setMode(650, 650) --set the window dimensions to 650 by 650
-  
-  
-  love.physics.setMeter(64) --the height of a meter our worlds will be 64px
-  world = love.physics.newWorld(0, 9.81*64, true) --create a world for the bodies to exist in with horizontal gravity of 0 and vertical gravity of 9.81
- 
-  objects = {} -- table to hold all our physical objects
- 
- 
-  --Create Platforms
-  objects.block1 = createPlatform(world, 200, 550, 50, 75, 5)
-  objects.block2 = createPlatform(world, 200, 400, 100, 50, 2)
-  objects.ground = createPlatform(world, love.graphics.getWidth() / 2, love.graphics.getHeight() - 25, love.graphics.getWidth(), 50, 10)
- 
-  objects.player = Player.create(world, 325, 325, 20, 20)
+	--initial graphics setup
+	love.graphics.setBackgroundColor(104, 136, 248) --set the background color to a nice blue
+	love.window.setMode(650, 650) --set the window dimensions to 650 by 650
+
+	love.physics.setMeter(64) --the height of a meter our worlds will be 64px
+	world = love.physics.newWorld(0, 9.81*64, true) --create a world for the bodies to exist in with horizontal gravity of 0 and vertical gravity of 9.81
+
+	objects = {} -- table to hold all our physical objects
+	objects.platforms = {} --table to hold all platforms
+
+	--Create Platforms
+	table.insert(objects.platforms, createPlatform(world, love.graphics.getWidth() / 2, love.graphics.getHeight() - 25, love.graphics.getWidth(), 50, 10, BASIC_PLATFORM))
+	table.insert(objects.platforms, createPlatform(world, 200, 550, 50, 75, 5, DISAPPEARING_PLATFORM))
+	table.insert(objects.platforms, createPlatform(world, 200, 400, 100, 50, 2, DISAPPEARING_PLATFORM))
+
+	objects.player = Player.create(world, 325, 325, 20, 20)
  
 end
- 
- 
+
 function love.update(dt)
-  world:update(dt) --this puts the world into motion
-  objects.player:update(dt)
+	world:update(dt) --this puts the world into motion
+	objects.player:update(dt)
+	local colliders = objects.player:getGroundedBodies()
+
+	for i = #objects.platforms, 1, -1 do
+		objects.platforms[i]:update(dt)
+		--if we are colliding with the current platform, then activate it
+		for j = 1, #colliders, 1 do
+			if colliders[j] == objects.platforms[i].fixture then
+				objects.platforms[i]:activate()
+			end
+		end
+		if objects.platforms[i].toDelete then
+			table.remove(objects.platforms, i)
+		end
+	end
 end
  
 function love.draw()
-  love.graphics.setColor(72, 160, 14) -- set the drawing color to green for the ground
-  love.graphics.polygon("fill", objects.ground.body:getWorldPoints(objects.ground.shape:getPoints())) -- draw a "filled in" polygon using the ground's coordinates
- 
-  love.graphics.setColor(193, 47, 14) --set the drawing color to red for the ball
-  love.graphics.polygon("fill", objects.player.body:getWorldPoints(objects.player.shape:getPoints()))
- 
-  love.graphics.setColor(50, 50, 50) -- set the drawing color to grey for the blocks
-  love.graphics.polygon("fill", objects.block1.body:getWorldPoints(objects.block1.shape:getPoints()))
-  love.graphics.polygon("fill", objects.block2.body:getWorldPoints(objects.block2.shape:getPoints()))
+
+	love.graphics.setColor(193, 47, 14) --set the drawing color to red for the ball
+	love.graphics.polygon("fill", objects.player.body:getWorldPoints(objects.player.shape:getPoints()))
+	
+	love.graphics.setColor(50, 50, 50) -- set the drawing color to grey for the blocks
+	for i = 1, #objects.platforms do
+		love.graphics.polygon("fill", objects.platforms[i].body:getWorldPoints(objects.platforms[i].shape:getPoints()))
+	end
+	
 end
--- platform.lua
+++ platform.lua
@@ -1,10 +1,47 @@
-function createPlatform(world, x, y, width, height, density)
+Platform = {}
+Platform.__index = Platform
+
+BASIC_PLATFORM = 1
+DISAPPEARING_PLATFORM = 2
+
+function createPlatform(world, x, y, width, height, density, platformType)
+	local block = {}
+	setmetatable(block, Platform)
+	
 	if density == nil then
 		density = 5
 	end
-	local block = {}
+	
+	block.toDelete = false
+	--variable to count down to deletion once a platform has been marked for deletion
+	block.deleteCountdown = -1
+	--amount of time after being stepped on until block disappears
+	block.deleteDuration = 1.5
+	block.platformType = platformType
 	block.body = love.physics.newBody(world, x, y, "static")
 	block.shape = love.physics.newRectangleShape(width, height)
-	block.fixture = love.physics.newFixture(block.body, block.shape, density) -- A higher density gives it more mass.
+	-- A higher density gives it more mass.
+	block.fixture = love.physics.newFixture(block.body, block.shape, density)
 	return block
+end
+
+function Platform:activate()
+	if self.platformType == BASIC_PLATFORM then
+		--print("doing nothing, it's a basic platform")
+	elseif self.platformType == DISAPPEARING_PLATFORM then
+		if self.deleteCountdown == -1 and self.toDelete ~= true then
+			print("make the platform DISAPPEAR!")
+			self.deleteCountdown = self.deleteDuration
+		end
+	end
+end
+
+function Platform:update(dt)
+	if self.deleteCountdown ~= -1  and self.deleteCountdown >= 0 then
+		self.deleteCountdown = self.deleteCountdown - dt
+	elseif self.deleteCountdown ~= -1 and self.deleteCountdown <= 0 then
+		self.deleteCountdown = -1
+		self.toDelete = true
+		self.body:destroy()
+	end
 end
